5a1b761ad8f85c40291d4e5c41ff81929614b119
YASH SAINI <yashsainishamli12@gmail.com>
2026-04-14 11:36:48 +0530

    add admin module for my project

--- Backend/src/modules/academic/academic.routes.js
+++ Backend/src/modules/academic/academic.routes.js
@@ -9,21 +9,21 @@ import {
   addStudent,
   removeStudent,
 } from "./academic.controller.js";
-import authMiddleware from "../../middlewares/auth.middleware.js";
-import roleMiddleware from "../../middlewares/role.middleware.js";
-
+import auth from "../../middlewares/auth.middleware.js";
+import role from "../../middlewares/role.middleware.js";
+import { ROLES } from "../../config/constants.js";
 const router = express.Router();
 
-router.use(authMiddleware);
+router.use(auth);
 
 // SUBJECT APIs
-router.post("/subjects", roleMiddleware("ADMIN", "TEACHER"), createSubject);
-router.get("/subjects", getSubjects);
-router.get("/subjects/:id", getSubjectById);
-router.get("/dashboard/:studentId", getStudentDashboard);
-router.put("/subjects/:id", roleMiddleware("ADMIN", "TEACHER"), updateSubject);
-router.delete("/subjects/:id", roleMiddleware("ADMIN"), deleteSubject);
-router.post("/subjects/:id/add-student", roleMiddleware("ADMIN", "TEACHER"), addStudent);
-router.post("/subjects/:id/remove-student", roleMiddleware("ADMIN", "TEACHER"), removeStudent);
+router.post("/subjects", auth, role(ROLES.ADMIN, ROLES.TEACHER), createSubject);
+router.get("/subjects", auth, role(ROLES.ADMIN, ROLES.STUDENT, ROLES.TEACHER), getSubjects);
+router.get("/subjects/:id", auth, role(ROLES.ADMIN, ROLES.STUDENT, ROLES.TEACHER),getSubjectById);
+router.get("/dashboard/:studentId",auth, role(ROLES.ADMIN, ROLES.STUDENT), getStudentDashboard);
+router.put("/subjects/:id", auth, role(ROLES.ADMIN, ROLES.TEACHER), updateSubject);
+router.delete("/subjects/:id", auth, role(ROLES.ADMIN), deleteSubject);
+router.post("/subjects/:id/add-student", auth, role(ROLES.ADMIN, ROLES.TEACHER), addStudent);
+router.post("/subjects/:id/remove-student", auth, role(ROLES.ADMIN, ROLES.TEACHER), removeStudent);
 
 export default router;
--- Backend/src/modules/ai/ai.routes.js
+++ Backend/src/modules/ai/ai.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import {chatWithAI, getStudentPerformance, getRecommendations, getWeakSubjects, getPerformanceTrend ,getSmartResources} from "./ai.controller.js";
 
 import auth from "../../middlewares/auth.middleware.js";
+import role from "../../middlewares/role.middleware.js";
+import { ROLES } from "../../config/constants.js";
 
 const router = express.Router();
 
-router.post("/chat", auth, chatWithAI);
+router.post("/chat", auth,role(ROLES.ADMIN,ROLES.STUDENT), chatWithAI);
 router.get("/performance/:studentId", auth, getStudentPerformance);
 router.get("/recommendations/:studentId", auth, getRecommendations);
 router.get("/weak-areas/:studentId", auth, getWeakSubjects);
