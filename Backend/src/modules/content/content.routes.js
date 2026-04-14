b9310b9eddb6c8f64030cc434bbce8c4f2cb4afc
YASH SAINI <yashsainishamli12@gmail.com>
2026-04-14 16:03:17 +0530

    add ACS in content module

--- Backend/src/modules/content/content.routes.js
+++ Backend/src/modules/content/content.routes.js
@@ -2,30 +2,35 @@ import express from 'express';
 import controller from '../content/content.controller.js'
 import auth from "../../middlewares/auth.middleware.js"
 import role from "../../middlewares/role.middleware.js"
+import { ROLES } from "../../config/constants.js"   
+import { auditMiddleware } from "../../middlewares/audit.middleware.js";
 import upload from "../../middlewares/upload.middleware.js"
-
 
 const router = express.Router();
 
-router.post("/", auth, role("TEACHER", "ADMIN"), upload.single("file"), controller.createContent);
 
-router.get("/", auth, controller.getAllContent);
-router.get("/:id", auth, controller.getContentById);
+router.post("/",auth,role(ROLES.TEACHER, ROLES.ADMIN),upload.single("file"),auditMiddleware("CREATE_CONTENT", "CONTENT"),controller.createContent);
 
-router.post("/:id/view", auth, controller.addView);
-router.post("/:id/download", auth, controller.addDownload);
+router.get("/",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),controller.getAllContent);
 
-router.delete("/:id", auth, role("TEACHER"), controller.deleteContent);
+router.get("/:id",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),controller.getContentById);
 
-router.post("/:id/bookmark", auth, controller.bookmarkContent);
-router.post("/:id/summarize", controller.summarizeContent);
-router.post("/:id/quiz", controller.generateQuizs);
-router.get("/:id/stream", controller.streamVideo)
-router.get("/:id/download", auth, controller.secureDownload)
-router.get("/:id/preview", controller.previewContent)
+router.post("/:id/view",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),controller.addView);
 
+router.post("/:id/download",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),controller.addDownload);
 
+router.delete("/:id",auth,role(ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("DELETE_CONTENT", "CONTENT"),controller.deleteContent);
 
+router.post("/:id/bookmark",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("BOOKMARK", "CONTENT"),controller.bookmarkContent);
 
+router.post("/:id/summarize",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("AI_SUMMARY", "CONTENT"),controller.summarizeContent);
+
+router.post("/:id/quiz",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("AI_QUIZ", "CONTENT"),controller.generateQuizs);
+
+router.get("/:id/stream",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),controller.streamVideo);
+
+router.get("/:id/download",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),controller.secureDownload);
+
+router.get("/:id/preview",auth,role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),controller.previewContent);
 
 export default router;
