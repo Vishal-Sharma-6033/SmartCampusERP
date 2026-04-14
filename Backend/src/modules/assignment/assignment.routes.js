0b003a75331abdd973c2d396ab77d0433fccaa94
YASH SAINI <yashsainishamli12@gmail.com>
2026-04-14 15:51:54 +0530

    add admin control system in assignment module

--- Backend/src/modules/assignment/assignment.routes.js
+++ Backend/src/modules/assignment/assignment.routes.js
@@ -2,17 +2,23 @@ import express from "express";
 import * as controller from "./assignment.controller.js";
 import auth from "../../middlewares/auth.middleware.js";
 import role from "../../middlewares/role.middleware.js";
-
+import { ROLES } from "../../config/constants.js";
+import { auditMiddleware } from "../../middlewares/audit.middleware.js";
 const router = express.Router();
 
-// Teacher
-router.post("/", auth, role("TEACHER","ADMIN"), controller.createAssignment);
-// Student
-router.post("/:id/submit", auth, role("STUDENT","ADMIN"), controller.submitAssignment);
-router.get("/student/:id",auth,role("STUDENT","ADMIN"),controller.getStudentAssignments);
 
-router.patch("/:submissionId/grade", auth, role("TEACHER","ADMIN"), controller.gradeSubmission);
-router.get("/:id/plagiarism", auth, role("TEACHER","ADMIN"), controller.plagiarismCheck);
-router.get("/:id/analytics", auth, role("TEACHER","ADMIN"), controller.analytics);
-router.get("/subject/:subjectId", auth, controller.subjectFeed);
+router.post("/",auth,role(ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("CREATE", "ASSIGNMENT"), controller.createAssignment);
+
+router.post("/:id/submit",auth,role(ROLES.STUDENT, ROLES.ADMIN),auditMiddleware("SUBMIT", "ASSIGNMENT"), controller.submitAssignment);
+
+router.get("/student/:id",auth,role(ROLES.STUDENT, ROLES.ADMIN, ROLES.TEACHER), controller.getStudentAssignments);
+
+router.patch("/:submissionId/grade",auth,role(ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("GRADE", "ASSIGNMENT"), controller.gradeSubmission);
+
+router.get("/:id/plagiarism",auth,role(ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("PLAGIARISM_CHECK", "ASSIGNMENT"), controller.plagiarismCheck);
+
+router.get("/:id/analytics",auth,role(ROLES.TEACHER, ROLES.ADMIN), controller.analytics);
+
+router.get("/subject/:subjectId",auth,role(ROLES.TEACHER, ROLES.ADMIN), controller.subjectFeed);
+
 export default router;
