099d41e24e41d2298c9c1ea8d2f134c7546fc651
YASH SAINI <yashsainishamli12@gmail.com>
2026-04-14 16:36:07 +0530

    add ACS in notifications and search module routes

--- Backend/src/modules/search/search.routes.js
+++ Backend/src/modules/search/search.routes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { search , recentSearches} from './search.controller.js';
 import role from "../../middlewares/role.middleware.js";
+import { ROLES } from "../../config/constants.js";
 import auth from "../../middlewares/auth.middleware.js";
+import { auditMiddleware } from "../../middlewares/audit.middleware.js";
 
 
 const router = express.Router();
 
-router.get('/', auth, search);
-router.get("/recent", auth, recentSearches);
+router.get('/', auth, role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("SEARCH", "SEARCH"), search);
+router.get("/recent", auth, role(ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN),auditMiddleware("RECENT_SEARCH", "SEARCH"), recentSearches);
+
 export default router;
