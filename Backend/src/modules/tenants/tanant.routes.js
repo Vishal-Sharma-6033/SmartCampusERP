c66a310dfbf5f5052e518404cf9e6b7447825652
Vishal Sharma <vs2520329@gmail.com>
2026-05-14 16:38:19 +0530

    Remove tenant module and duplicate socket file

--- Backend/src/modules/tenants/tanant.routes.js
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-// import { createTenantController } from "./tenant.controller.js";
-
-// const router = express.Router();
-
-// router.post("/", createTenantController); 
-
-
-// export default router;
